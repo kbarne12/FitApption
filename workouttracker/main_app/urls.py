f9f4b038c9c99d7bb711b5550978bbaee411eb23
kbarne12 <kbarne12@gmail.com>
2021-05-07 15:33:57 -0400

    signup template

--- workouttracker/main_app/urls.py
+++ workouttracker/main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.about, name='about'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- workouttracker/main_app/views.py
+++ workouttracker/main_app/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Define the home view
 def about(request):
   return render(request, 'about.html')
 
-# Create your views here.
+
+
+
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+
