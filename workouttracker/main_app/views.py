282b7fee79f9f27e78361813b784afb12cb4e27e
kbarne12 <kbarne12@gmail.com>
2021-05-07 15:38:15 -0400

    auth is complete

--- workouttracker/main_app/views.py
+++ workouttracker/main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Define the home view
 def about(request):
